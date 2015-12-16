a12a40d4c10fe8cc49c2561b2bd60dd86c348c5c
Thomas Cousin <thomas.cousin@wescale.fr>
2015-12-16 10:47:19 +0100

    Remove unnecessary stuff

--- app/app.php
+++ app/app.php
@@ -27,47 +27,6 @@ $app->get('/version', function () use ($app) {
 });
 
 
-// Zen Statement From GitHub
-// -----------------------------------------------------------------------------
-// Can be used to verify that the application has external connectivity.
-$app->get('/zen', function () use ($app) {
-    $response = $app->curl->get('https://api.github.com/zen');
-    if ($response->headers['Status-Code'] != 200) {
-        $app->halt(502, 'GitHub has failed with :' + $response->headers['Status-Code']);
-    }
-    $app->response->write($response->body);
-});
-
-
-// Fetch a file from the file store.
-// -----------------------------------------------------------------------------
-// Authenticated request for a file from the file store
-$app->get('/files/:filename', $authenticate($app), function ($filename) use ($app) {
-    $supported_types = (object) array(
-        'json'    => 'application/json',
-        'xml'     => 'application/xml',
-        'csv'     => 'text/csv',
-        'unknown' => 'application/octet-stream'
-    );
-
-    $filename  = pathinfo($filename, PATHINFO_BASENAME);
-    $extension = pathinfo($filename, PATHINFO_EXTENSION);
-    $path      = realpath(__DIR__ . '/../file_store/' . $filename);
-
-    $content_type
-        = property_exists($supported_types, $extension)
-        ? $supported_types->$extension
-        : $supported_types->unknown;
-
-    if (!is_readable($path)) {
-        $app->notFound();
-    }
-
-    $app->response->headers->set('Content-Type', $content_type);
-    readfile($path);
-});
-
-
 // Say hello to a user
 // -----------------------------------------------------------------------------
 // Used to test parameters from [issue 4](https://github.com/there4/slim-unit-testing-example/issues/4).
@@ -83,18 +42,6 @@ $app->map('/say-hello', function () use ($app) {
     $app->response->write($response);
 })->via('POST', 'PUT');
 
-
-$app->post('/issue3', function () use ($app) {
-    $name = $app->request->post('name');
-    $response = $name ? 'Hello ' . $name : 'Missing parameter for name';
-    $app->response->write($response);
-});
-
-$app->get('/issue12', function () use ($app) {
-    $referer = $app->request->headers('referer');
-    $response = $referer ? $referer : 'Missing referer header';
-    $app->response->write($response);
-});
 
 
 /* End of file app.php */
